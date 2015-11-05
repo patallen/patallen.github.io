3b3bd918edfdb30c44c111e351c61a51fb6ea09f
Patrick Allen <prallen90@gmail.com>
2015-11-05 14:11:02 -0500

    Clean up for pep8

--- pelicanconf.py
+++ pelicanconf.py
@@ -28,7 +28,7 @@ LINKS = ()
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 5
+DEFAULT_PAGINATION = 4
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
